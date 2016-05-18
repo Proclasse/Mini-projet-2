cdc854c03aa51e42a07d47a07cb8ca7c1b56ff7f
Proclasse <prloccase2@hotmail.com>
2016-05-18 15:37:46 +0200

    modif des noms de photos plus insertions des liens dans bdd

--- Doudous.php
+++ Doudous.php
@@ -6,23 +6,44 @@
 	<?php
 	include 'menu.inc.php';
 	?>
-	
-<?php
-	try {
-		$bdd = new PDO('mysql:host=localhost;dbname=mini-projet','root','');
-	}
-	catch(PDOException $e) {
-		echo "Problème de connexion à la base de données";
-	}
 
-?>	
+	<!-- Connexion à la bdd -->
+	<?php 
 	
+		try {
+			$bdd = new PDO('mysql:host=localhost;dbname=mini-projet','root','');
+		// gestion du niveau d'erreur
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		// requête dans la variable $sql puis injection de la requête dans la base, le tout dans la variable $req
+		$sql = 'SELECT liens FROM categories WHERE id=1';
+		$req = $bdd -> query($sql);
+		
+		$req -> setFetchMode(PDO::FETCH_ASSOC);
+		
+			foreach ($req as $u) {
+				echo "
+						<section>
+						<h1>Les doudous</h1>
+						<img src=".$u." />
+						</section>
 	
-	<section>
-		<h1>Les doudous</h1>
-	</section>
-	
-</div>
+					</div>
+				";
+				
+				
+				print_r($u);
+			}
+					
+		}
+		
+		catch(PDOException $e) {
+			echo "Problème de connexion à la base de données :".$e->getMessage();
+			die();
+		}
+
+	?>
+
 <?php
    include 'footer.inc.php';
 ?>
--- Jupes.php
+++ Jupes.php
@@ -14,3 +14,4 @@
 </div>
 <?php
    include 'footer.inc.php';
+?>
