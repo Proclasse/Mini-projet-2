3258932b0e3e5044d886045c45a516be37803492
Proclasse <prloccase2@hotmail.com>
2016-05-23 21:13:44 +0200

    creation page adminCM pour backoffice plus modifs footer et accueil et css

--- accueil.php
+++ accueil.php
@@ -9,12 +9,12 @@ include 'head.inc.php';
 	?>
 	
 	<section>
-		<h1>Présentation de l'auteure</h1>
+		<h1>Qui suis-je?</h1>
 		<img id="image_presentation" src="image_fond/s-couverture.jpg" />
-		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ligula metus, mollis a turpis sed, ornare faucibus sapien. Vivamus sit amet quam sem. Praesent ac lorem condimentum, lacinia ex vitae, rutrum nulla. Quisque pellentesque, arcu condimentum facilisis mollis, lorem orci dignissim lacus, vel sollicitudin orci est in lorem. Suspendisse potenti. Duis urna massa, gravida at libero eu, vestibulum ornare sapien. Vestibulum non suscipit ligula. Nunc maximus dolor a ornare pharetra. Phasellus non tellus lacus. Mauris cursus imperdiet lacinia. Praesent tristique urna tortor, vitae suscipit augue imperdiet at. Ut congue neque sit amet lacus accumsan luctus. Pellentesque a elementum tortor.</p>
-		<p>Curabitur est dolor, luctus sit amet ante in, molestie eleifend nisl. Morbi lobortis mauris quis massa interdum pretium. Integer mi enim, dictum non dictum eget, ullamcorper vel lacus. Aliquam magna nisi, fermentum vel ipsum in, dictum accumsan odio. Mauris porta eros non metus tempus, sit amet aliquet felis pellentesque. Sed a porttitor sem, at maximus metus. Nulla hendrerit lectus vitae turpis bibendum scelerisque. Sed rhoncus arcu at tellus efficitur, quis iaculis justo tincidunt. Maecenas elementum ligula quis consectetur viverra. Nam eu vestibulum risus, non euismod massa. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet massa commodo, consequat lacus faucibus, congue tortor.</p>
-		<p>Suspendisse dignissim ornare justo ac facilisis. Aliquam scelerisque porttitor nisl nec lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent tempor euismod nulla, non sagittis risus pellentesque eu. Nullam convallis pretium tempor. Mauris eget lacus pretium, pretium justo a, volutpat nibh. Cras vestibulum odio elit, commodo volutpat arcu consequat et. In at felis pharetra, imperdiet nisi non, varius eros.</p>
-		<p>Aliquam libero eros, scelerisque sit amet augue sed, gravida suscipit enim. Nunc consectetur diam nec nibh sodales, eleifend blandit metus ornare. Mauris ultricies tellus non cursus sodales. Integer vulputate eu massa at egestas. Duis sed scelerisque dolor. Fusce in sapien nunc. Ut metus turpis, aliquet vel feugiat id, mollis faucibus justo. Nunc mi dui, pulvinar at libero nec, congue faucibus lectus. Cras sed lorem at leo ornare vestibulum. Quisque est lectus, egestas eget porttitor in, maximus in nunc. </p>
+		<p class="alinea"> Passionnée par la création depuis toute petite, je me suis dirigée naturellement vers la couture. Autodidacte, j'ai commencé par retoucher mes propres vêtements puis à confectionner des pièces à partir de patrons.</p>
+		<p class="alinea">Plus tard, j'ai eu envie de créer mes propres modèles, essentiellement des sarouels, me laissant plein de possibilités pour mes futures réalisations.</p>
+ 		<p class="alinea">Depuis 2002, acquisition de ma première machine, j'ai agrandi mon parc personnel d'outils, ustensiles et machines en tout genre afin de varier les styles et de proposer des finitions de meilleure qualité.</p>
+		<p class="alinea">Auto-entrepreneuse depuis 2011, je suis à l'écoute de vos désirs et commandes, je vous proposerai des ouvrages de qualité, de fabrication artisanale, chaque pièce étant unique et originale. </p>
 	</section>
 
 </div>
--- footer.inc.php
+++ footer.inc.php
@@ -21,9 +21,8 @@
 			</div>
 	
 			<div class="bloc_footer">
-					Retrouver nous sur : <br />
+					Retrouvez-moi sur : <br />
 					<a href="https://www.facebook.com/La-PloK-PiK-264384806950071/?fref=ts" target="_blank"><img src="images/facebook.png"/></a>
-					<a href="https://twitter.com/?lang=fr" target="_blank"> <img src="images/twitter.png" alt="logos twitter facebook"/></a>
 			</div>
 		</span>
 	</nav>
