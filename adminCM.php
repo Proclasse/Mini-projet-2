9494ca5ed49080a9da83f8b0959b9fb9ae00c494
Proclasse <prloccase2@hotmail.com>
2016-05-24 10:54:40 +0200

    modif admin CM

--- adminCM.php
+++ adminCM.php
@@ -11,7 +11,7 @@ echo '
 <section>
 	<div id="webmaster">
 		<script src="contact.js" type="text/javascript"></script> <!-- à prévoir pour la suite : si les valeurs de co sont bonnes, alors la valeur l\'attribut css de l\'id webmaster passe à display: none afin d\'effacer le formulaire -->
-		<h2>Page réservée à l\'administrateur du site.</h2>		<!-- certainement et donc plus facilement à faire en php... -->
+		<h2>Page réservée à l\'administrateur du site.</h2>		<!-- certainement (et donc) plus facile à faire en php... -->
 		<form method="post">
 			<label>Pseudo
 				<input type="text" name="pseudo"/>
